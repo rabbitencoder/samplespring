9f61a0d2fc471b35d746f1f7bf986ff0c456163c
rabbitencoder <rabbitencoder@gmail.com>
2023-08-29 11:18:20 +0530

    Added the comment

--- src/main/java/com/rabbitencoder/restservices/SamplespringApplication.java
+++ src/main/java/com/rabbitencoder/restservices/SamplespringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SamplespringApplication {
 
+    //Main Code Changes
     public static void main(String[] args) {
         SpringApplication.run(SamplespringApplication.class, args);
     }
